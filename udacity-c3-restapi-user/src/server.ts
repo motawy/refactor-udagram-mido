45b7c586147d9aa167d47837e37deb75d96a554c
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-23 13:58:58 +0100

    Hotfix - installed cors package

--- udacity-c3-restapi-user/src/server.ts
+++ udacity-c3-restapi-user/src/server.ts
@@ -4,7 +4,7 @@ import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
 import { config } from './config/config';
 import { V0MODELS } from './controllers/v0/model.index';
-
+import cors from 'cors';
 const c = config.dev;
 
 (async () => {
@@ -16,22 +16,8 @@ const c = config.dev;
 
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
   // Enable CORS
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Max-Age", "1000000000");
-    next();
-  });
+  app.use(cors());
 
   app.use('/api/v0/', IndexRouter)
 
