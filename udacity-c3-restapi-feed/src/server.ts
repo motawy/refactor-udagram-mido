b762cbd8a7bccda7977630462b495729d1d131e3
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-23 17:41:17 +0100

    Hotfix - cors

--- udacity-c3-restapi-feed/src/server.ts
+++ udacity-c3-restapi-feed/src/server.ts
@@ -13,21 +13,27 @@ const c = config.dev;
 (async () => {
   await sequelize.addModels(V0MODELS);
   await sequelize.sync();
-
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
 
+  let allowedOrigins = 'http://localhost:8100'
+  app.use(cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins !== origin) {
+        return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
+      }
+      return callback(null, true);
+    }
+  }));
+
   app.use(bodyParser.json());
-
-  app.use(cors())
-
   app.use('/api/v0/', IndexRouter)
-
   // Root URI call
   app.get("/", async (req, res) => {
     res.send("/api/v0/");
   });
-
 
   // Start the Server
   app.listen(port, () => {
