757335dfa6e0dd933507231fbb334e0b17e7cef8
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-23 12:35:58 +0100

    Hotfix - adding headers for cors

--- udacity-c3-restapi-user/src/server.ts
+++ udacity-c3-restapi-user/src/server.ts
@@ -23,6 +23,16 @@ const c = config.dev;
     next();
   });
 
+  // Enable CORS
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Max-Age", "1000000000");
+    next();
+  });
+
   app.use('/api/v0/', IndexRouter)
 
   // Root URI call
